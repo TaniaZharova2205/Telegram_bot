0c6a05dafbbbb5e0e1e845b78bc2b7dc324ed5fa
tanya_zharova <tazharova@cate.ru>
2025-01-21 08:01:02 +0300

    config

--- config.py
+++ config.py
@@ -5,8 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Чтение токена из переменной окружения
-TOKEN = "TOKEN"
+TOKEN = os.getenv("TOKEN")
 
-KEY_WETHER = 'KEY_WETHER'
+if not TOKEN:
+    raise ValueError("⚠️ Токен не найден! Убедитесь, что переменная окружения 'TOKEN' настроена.")
 
-DATABASE = 'DATABASE'
+KEY_WETHER = os.getenv("KEY_WETHER")
+
+if not KEY_WETHER:
+    raise ValueError("⚠️ Ключ с погодой не найден! Убедитесь, что переменная окружения 'KEY_WETHER' настроена.")
+
+DATABASE = os.getenv("DATABASE")
+
+if not DATABASE:
+    raise ValueError("⚠️ База данных не найдена! Убедитесь, что переменная окружения 'DATABASE' настроена.")
